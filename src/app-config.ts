2d2239d497c29a422a2175a078e632f5de7d6a40
Maximilian Krüger <Maximilian.Krueger@airsphere.de>
2022-08-05 15:22:08 +0200

    lint application
    * to follow all rules lint the application

--- src/app-config.ts
+++ src/app-config.ts
@@ -1,10 +1,9 @@
-interface AppConfig{
-    TitelRegex: string;
-    BodyRegex: string;
-    OnFailedTitelComment: string;
-    OnFailedBodyComment: string;
-    FailActionOnFailedRegex: boolean;
-    RequestChangesOnFailedRegex: boolean;
-    CreateReviewOnFailedRegex: boolean;
-
+interface AppConfig {
+  TitelRegex: string;
+  BodyRegex: string;
+  OnFailedTitelComment: string;
+  OnFailedBodyComment: string;
+  FailActionOnFailedRegex: boolean;
+  RequestChangesOnFailedRegex: boolean;
+  CreateReviewOnFailedRegex: boolean;
 }
